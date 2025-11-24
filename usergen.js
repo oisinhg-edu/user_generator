ea53a15ac4c4f7d34881e103aebb8c6af4a8af74
Oisin HG <N00242930@iadt.ie>
2025-11-24 03:40:29 +0000

    Moved some more logic to helper functions. Pushing to array before writing to file instead of using string concatenation.

--- usergen.js
+++ usergen.js
@@ -21,7 +21,7 @@ const domains = loadedDomains.split('\n').map(line => line.trim()).filter(line =
 
 const alphaNumeric = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
-const thirty_day_months = [4, 6, 9, 11];
+const thirtyDayMonths = [4, 6, 9, 11];
 
 const bioHobbies = hobbyList.map(r => r['Hobby-name']);
 
@@ -51,64 +51,56 @@ const bioEndings = [
 // use argument when running script
 const numUsers = parseInt(process.argv[2]) || 1; // default 1
 
-function gen_user_data() {
+function genUserData() {
 
     let username = getUniqueUsername();
 
-    let email = gen_email();
+    let email = genEmail();
 
-    let password = gen_password_string();
+    let password = genPasswordString();
 
-    let bio = gen_bio();
+    let bio = genBio();
 
-    let joined_at = gen_timestamp_string();
+    let joined_at = genTimestampString();
 
-    let dob = gen_date_string(1920, 2011);
+    let dob = genDateString(1920, 2011);
 
     // 15% chance for a premium user
     let account_status = Math.random() < 0.15 ? 'premium' : 'standard';
 
-    let user_data = `('${username}', '${email}', '${password}', '${bio}.', '${joined_at}', '${dob}', '${account_status}')`;
+    let userData = `('${username}', '${email}', '${password}', '${bio}.', '${joined_at}', '${dob}', '${account_status}')`;
 
-    return user_data;
+    return userData;
 }
 
-function gen_username() {
+function genUsername() {
     let base = uniqueNamesGenerator({
         dictionaries: [adjectives, names],
         separator: '',
         style: 'capital'
     });
 
-    let suffix = '';
 
-    for (let i = 0; i < 3; i++) {
-        suffix += choose_from_array(alphaNumeric);
-    }
+    let suffix = randomString(3);
 
     return base + suffix;
 }
 
 function getUniqueUsername() {
     let name;
-    
+
     do {
-        name = gen_username();
+        name = genUsername();
     }
     while (seenName.has(name));
-    
+
     seenName.add(name);
 
     return name;
 }
 
-function gen_email() {
-    let local = '';
-
-    for (let i = 0; i < 8; i++) {
-        let val = choose_from_array(alphaNumeric);
-        local += val;
-    }
+function genEmail() {
+    let local = randomString(8);
 
     let domain = domains[Math.floor(Math.random() * domains.length)];
     let email = `${local}` + '@' + `${domain}`;
@@ -116,35 +108,44 @@ function gen_email() {
     return email;
 }
 
-function gen_timestamp_string() {
-    let date = gen_date_string(2010, 2025);
+function genPasswordString() {
+    let hash = randomString(22);
+    let seed = randomString(31);
+
+    let password = '$2y$10$' + hash + seed;
+    return password;
+}
+
+function genBio() {
+    let adj = uniqueNamesGenerator({
+        dictionaries: [adjectives]
+    });
+
+    let hobby = chooseFromArray(bioHobbies);
+    let ending = chooseFromArray(bioEndings);
+
+    return `I am a ${adj} person who enjoys ${hobby}, ${ending}`;
+}
+
+function genTimestampString() {
+    let date = genDateString(2010, 2025);
 
     let hour = Math.floor(Math.random() * 24);
     let mins = Math.floor(Math.random() * 60);
     let secs = Math.floor(Math.random() * 60);
 
-    if (hour < 10) {
-        hour = '0' + hour;
-    }
-    if (mins < 10) {
-        mins = '0' + mins;
-    }
-    if (secs < 10) {
-        secs = '0' + secs;
-    }
-
-    let timestamp = `${date} ${hour}:${mins}:${secs}`;
+    let timestamp = `${date} ${padZero(hour)}:${padZero(mins)}:${padZero(secs)}`;
     return timestamp;
 }
 
-function gen_date_string(min_year, max_year) {
+function genDateString(min_year, max_year) {
     let max_day = 31;
 
     let year = Math.floor(Math.random() * (max_year - min_year + 1) + min_year);
 
-    let month = Math.floor(Math.random() * (12 - 1) + 1);
+    let month = Math.floor(Math.random() * (12) + 1);
 
-    if (thirty_day_months.includes(month)) {
+    if (thirtyDayMonths.includes(month)) {
         max_day = 30;
     }
     else if (month == 2) {
@@ -153,62 +154,39 @@ function gen_date_string(min_year, max_year) {
 
     let day = Math.floor(Math.random() * (max_day) + 1);
 
-    if (month < 10) {
-        month = '0' + month;
-    }
-    if (day < 10) {
-        day = '0' + day;
-    }
-
-    let date = `${year}-${month}-${day}`;
+    let date = `${year}-${padZero(month)}-${padZero(day)}`;
     return date;
 }
 
-function gen_password_string() {
-    let hash = '';
-
-    for (let i = 0; i < 22; i++) {
-        let val = choose_from_array(alphaNumeric);
-        hash += val;
-    }
-
-    let seed = '';
-
-    for (let i = 0; i < 31; i++) {
-        let val = choose_from_array(alphaNumeric);
-        seed += val;
-    }
-
-    let password = '$2y$10$' + hash + seed;
-    return password;
-}
-
-function gen_bio() {
-    let adj = uniqueNamesGenerator({
-        dictionaries: [adjectives]
-    });
-
-    let hobby = choose_from_array(bioHobbies);
-    let ending = choose_from_array(bioEndings);
-
-    return `I am a ${adj} person who enjoys ${hobby}, ${ending}`;
-}
-
 // pick one value from an array or a string
-function choose_from_array(arr) {
+function chooseFromArray(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
+// adds a leading 0 to a number if it is below 10
+function padZero(n) {
+    return n < 10 ? '0' + n : n;
+}
+
+// creates a random string of given length 
+// default alphabet is the alphaNumeric array
+function randomString(length, alphabet = alphaNumeric) {
+    let s = '';
+    for (let i = 0; i < length; i++) {
+        s += chooseFromArray(alphabet);
+    }
+    return s;
+}
+
 if (numUsers === 1) {
-    fs.writeFileSync('users.sql', gen_user_data());
+    fs.writeFileSync('users.sql', genUserData());
 }
 else {
-    let outputStr = '';
+    let outputArr = [];
 
-    for (let i = 0; i < numUsers - 1; i++) {
-        outputStr += gen_user_data() + ',\n';
+    for (let i = 0; i < numUsers; i++) {
+        outputArr.push(genUserData());
     }
 
-    outputStr += gen_user_data();
-    fs.writeFileSync('users.sql', outputStr);
+    fs.writeFileSync('users.sql', outputArr.join(',\n'));
 }
